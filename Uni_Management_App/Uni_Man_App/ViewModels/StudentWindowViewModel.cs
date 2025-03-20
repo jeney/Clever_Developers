6c32f09f18452021ce23f4209ebdd65676cf6be6
denis-nedret <denis.nedret0@gmail.com>
2025-03-20 19:02:42 +0100

    Sidebar menu for the student window is made.

---
+++ Uni_Management_App/Uni_Man_App/ViewModels/StudentWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+
+namespace Uni_Man_App.ViewModels;
+
+public partial class StudentWindowViewModel : ViewModelBase
+{
+    [ObservableProperty] private bool _isPaneOpen = true;
+
+    [ObservableProperty] private ViewModelBase _currentPage = new HomeViewModel();
+    
+    
+    [ObservableProperty] private ListItemTemplate? _selectedListItem;
+
+    partial void OnSelectedListItemChanged(ListItemTemplate? value)
+    {
+        if (value is null) return;
+        var instance = Activator.CreateInstance(value.ModelType);
+        if (instance is null) return;
+        CurrentPage = (ViewModelBase)instance;
+    }
+
+    [RelayCommand]
+    private void TriggerPane()
+    {
+        IsPaneOpen = !IsPaneOpen;
+    }
+
+    public ObservableCollection<ListItemTemplate> Items { get; } = new()
+    {
+        new ListItemTemplate(typeof(HomeViewModel)),
+        new ListItemTemplate(typeof(EnrollmentsViewModel)),
+        new ListItemTemplate(typeof(SubjectsViewModel)),
+    
+    
+    };
+
+
+}
+public class ListItemTemplate
+{
+    public ListItemTemplate(Type type)
+    {
+        ModelType = type;
+        Label = type.Name.Replace("ViewModel", "");
+    }
+    public string Label { get; }
+    public Type ModelType { get; }
+}
