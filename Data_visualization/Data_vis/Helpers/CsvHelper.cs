223dfd5c6167630be300d294af3b466f08a9a0e5
denis-nedret <denis.nedret0@gmail.com>
2025-04-05 00:59:36 +0200

    The csv helper is working.

---
+++ Data_visualization/Data_vis/Helpers/CsvHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace Data_vis.Helpers;
+
+using System;
+using CsvHelper;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+using Data_vis.Models;
+
+	public class CsvReaderHelper
+	{
+		public void ReadCsvFile()
+		{
+			using (var streamReader = new StreamReader(@"Assets/VideoGamesSales.csv.csv"))
+			using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+			{
+				csvReader.Context.RegisterClassMap<ClassMap>();
+				var records = csvReader.GetRecords<VideoGameSales>().ToList();
+
+				// foreach (var record in records)
+				// {
+				// 	Console.WriteLine($"{record.Name} - {record.Platform} ({record.Year})");
+				// }
+			}
+		}
+	}
+
+// reads the csv file
+// converts each row into a VideoGameSales instance using ClassMap.cs
