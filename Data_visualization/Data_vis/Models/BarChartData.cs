11e787f393a70fc65062d2993f521a975ef3cc09
Ors Jeney <ors@jeney.hu>
2025-04-06 21:33:15 +0200

    Added the basic queries

---
+++ Data_visualization/Data_vis/Models/BarChartData.cs
@@ -0,0 +1,11 @@
+namespace LINQ;
+public class BarChartData
+{
+	public float Number;
+	public string Name;
+	public BarChartData(float number, string name)
+	{
+		Number = number;
+		Name = name;
+	}
+}
---
+++ Data_visualization/Data_vis/Models/PieChartData.cs
@@ -0,0 +1,11 @@
+namespace LINQ;
+public class PieChartData
+{
+	public float Number;
+	public string Name;
+	public PieChartData(float number, string name)
+	{
+		Number = number;
+		Name = name;
+	}
+}
